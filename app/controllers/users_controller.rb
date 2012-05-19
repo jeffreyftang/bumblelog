419bddc1e8ae175570ee01d1f6bfd5f199995237
Jeffrey Tang <jtangbackup@gmail.com>
2012-05-19 09:53:27 -0500

    Added title method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
   
   def new
   	@user = User.new
+  	@title = 'Sign up'
   end
   
   def show
   	@user = User.find(params[:id])
+  	@title = @user.get_name
   end
   
   def create
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
 
+	def title
+		default_title = 'The Bumblelog'
+		if @title.nil?
+			default_title
+		else
+			@title
+		end
+	end
+
 end
