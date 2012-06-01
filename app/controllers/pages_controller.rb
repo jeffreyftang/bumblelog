fa2ffe7f8b0ed8df562e60dfe12f25829a537173
Jeffrey Tang <jtangbackup@gmail.com>
2012-05-31 21:14:07 -0500

    initial homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,23 @@
 class PagesController < ApplicationController
 
+	layout :choose_layout
+
 	before_filter :ensure_logged_in
 
   def home
+  	@title = 'Welcome'
   end
 
   def blog
   	@posts = Post.all.delete_if { |p| !p.published? }
   end
-
-  def cpanel
+  
+  def choose_layout
+  	if action_name == 'home'
+  		'home_layout'
+  	else
+  		'application'
+  	end
   end
 
 end
